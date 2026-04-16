7c5d9a2d68e23dc4888c91b9aed2c68408c858f8
Jérémy Audiger <jeremy.audiger@icloud.com>
2026-04-16 21:09:20 +0200

    build: update Rust to 1.95
    Signed-off-by: Jérémy Audiger <jeremy.audiger@icloud.com>

--- src/main.rs
+++ src/main.rs
@@ -7,46 +7,48 @@
 
 mod app;
 
-#[cfg(target_arch = "wasm32")]
-use eframe::wasm_bindgen::prelude::*;
+std::cfg_select! {
+    target_arch = "wasm32" => {
+        use eframe::wasm_bindgen::prelude::*;
 
-#[cfg(not(target_arch = "wasm32"))]
-fn main() -> eframe::Result {
-    let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([1400.0, 350.0])
-            .with_resizable(false),
-        ..Default::default()
-    };
+        fn main() {
+            tracing_wasm::set_as_global_default();
 
-    eframe::run_native(
-        "Brainfuck GUI",
-        native_options,
-        Box::new(|cc| Ok(Box::new(app::BrainfuckGui::new(cc)))),
-    )
-}
+            wasm_bindgen_futures::spawn_local(async {
+                let canvas = eframe::web_sys::window()
+                    .unwrap()
+                    .document()
+                    .unwrap()
+                    .get_element_by_id("egui-canvas")
+                    .unwrap()
+                    .dyn_into::<eframe::web_sys::HtmlCanvasElement>()
+                    .unwrap();
 
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    tracing_wasm::set_as_global_default();
+                eframe::WebRunner::new()
+                    .start(
+                        canvas,
+                        eframe::WebOptions::default(),
+                        Box::new(|cc| Ok(Box::new(app::BrainfuckGui::new(cc)))),
+                    )
+                    .await
+                    .unwrap();
+            });
+        }
+    }
+    _ => {
+        fn main() -> eframe::Result {
+            let native_options = eframe::NativeOptions {
+                viewport: egui::ViewportBuilder::default()
+                    .with_inner_size([1400.0, 350.0])
+                    .with_resizable(false),
+                ..Default::default()
+            };
 
-    wasm_bindgen_futures::spawn_local(async {
-        let canvas = eframe::web_sys::window()
-            .unwrap()
-            .document()
-            .unwrap()
-            .get_element_by_id("egui-canvas")
-            .unwrap()
-            .dyn_into::<eframe::web_sys::HtmlCanvasElement>()
-            .unwrap();
-
-        eframe::WebRunner::new()
-            .start(
-                canvas,
-                eframe::WebOptions::default(),
+            eframe::run_native(
+                "Brainfuck GUI",
+                native_options,
                 Box::new(|cc| Ok(Box::new(app::BrainfuckGui::new(cc)))),
             )
-            .await
-            .unwrap();
-    });
+        }
+    }
 }
